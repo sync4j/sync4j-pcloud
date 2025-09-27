eb1f49ad22eb3dbfdc98cc6f97bc85cb8e624b1e
jma <jma@UM790Linux>
2025-09-27 10:52:45 +0200

    Adapts to sync4j changes

--- src/test/java/com/fathzer/sync4j/pcloud/test/SynchroTest.java
+++ src/test/java/com/fathzer/sync4j/pcloud/test/SynchroTest.java
@@ -12,9 +12,10 @@ import com.fathzer.sync4j.sync.parameters.SyncParameters;
 public class SynchroTest {
     public static void main(String[] args) throws IOException {
         SyncParameters params = new SyncParameters();
-        params.getPerformance().setFastList(true);
-        params.getPerformance().setMaxComparisonThreads(8);
-        params.getPerformance().setMaxTransferThreads(5);
+        params.performance().fastList(true).
+            maxCopyThreads(8).
+            maxComparisonThreads(5);
+        params.dryRun(true);
 
         try (PCloudProvider provider = new PCloudProvider(Zone.US, System.getenv("PCLOUD_TOKEN"))) {
             Folder source = provider.get("/PhotosJM/2002").asFolder();
