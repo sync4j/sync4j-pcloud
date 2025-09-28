eb35439153fc93685def6aa392c2f5d75a91581f
jma <jma@UM790Linux>
2025-09-28 12:02:04 +0200

    Adapts to Synchronizer changes

--- src/test/java/com/fathzer/sync4j/pcloud/test/SynchroTest.java
+++ src/test/java/com/fathzer/sync4j/pcloud/test/SynchroTest.java
@@ -1,7 +1,5 @@
 package com.fathzer.sync4j.pcloud.test;
 
-import java.io.IOException;
-
 import com.fathzer.sync4j.Folder;
 import com.fathzer.sync4j.file.LocalProvider;
 import com.fathzer.sync4j.pcloud.PCloudProvider;
@@ -10,7 +8,7 @@ import com.fathzer.sync4j.sync.Synchronizer;
 import com.fathzer.sync4j.sync.parameters.SyncParameters;
 
 public class SynchroTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         SyncParameters params = new SyncParameters();
         params.performance().fastList(true).
             maxCopyThreads(8).
@@ -20,8 +18,10 @@ public class SynchroTest {
         try (PCloudProvider provider = new PCloudProvider(Zone.US, System.getenv("PCLOUD_TOKEN"))) {
             Folder source = provider.get("/PhotosJM/2002").asFolder();
             Folder target = LocalProvider.INSTANCE.get("/home/jma/tmp/photosTest/2002").asFolder();
-            Synchronizer synchronizer = new Synchronizer(source, target, params);
-            synchronizer.run();
+            try (Synchronizer synchronizer = new Synchronizer(source, target, params)) {
+                synchronizer.start();
+                synchronizer.waitFor();
+            }
         }
     }
 }
