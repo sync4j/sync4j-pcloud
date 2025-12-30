1ebcd8886bc1a16af5074f0bf6c8941dfbfb775f
Fathzer <jm-dev@astesana.net>
2025-12-30 12:11:29 +0100

    Try to fix Sonar complaints

--- src/main/java/com/fathzer/sync4j/pcloud/PCloudFile.java
+++ src/main/java/com/fathzer/sync4j/pcloud/PCloudFile.java
@@ -2,6 +2,7 @@ package com.fathzer.sync4j.pcloud;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 import com.fathzer.sync4j.File;
 import com.fathzer.sync4j.Folder;
@@ -13,8 +14,9 @@ import jakarta.annotation.Nullable;
 
 class PCloudFile extends PCloudEntry implements File {
 
-    PCloudFile(@Nullable String parentPath, @Nullable Folder parent, @Nonnull RemoteEntry remoteEntry, @Nonnull PCloudProvider provider) {
+    PCloudFile(@Nonnull String parentPath, @Nullable Folder parent, @Nonnull RemoteEntry remoteEntry, @Nonnull PCloudProvider provider) {
         super(parentPath, parent, remoteEntry, provider);
+        Objects.requireNonNull(parentPath);
         if (!remoteEntry.isFile()) {
             throw new IllegalArgumentException("Not a file");
         }
